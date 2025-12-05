474f8d46d418667bdc461b2da9cc141fa8e6d70f
yinxianwei <yinxianwei@qq.com>
2025-12-05 10:36:18 +0800

    update

--- pelicanconf.py
+++ pelicanconf.py
@@ -38,10 +38,19 @@ ANALYTICS = """
         link.textContent = "豫ICP备16018939号-2"
         document.querySelector('#contentinfo > p').prepend(link)
     </script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-J8HMB6HPES"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-J8HMB6HPES');
+    </script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1314323945395441"
+     crossorigin="anonymous"></script>
 """
 GITHUB_URL = "https://github.com/yinxianwei"
 DEFAULT_CATEGORY = "随笔"
 THEME_TEMPLATES_OVERRIDES = ["templates"]
 STYLESHEET_URL = "custom.css"
-# Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
