e5b1bdd981822f84aeb6bf24f6e2064c89b8d2e1
yinxianwei <yinxianwei@qq.com>
2026-08-17 15:46:07 +0800

    Revert "update"
    This reverts commit c3478aa7c26aac5fb13ed0ac2da31a6942f118cf.

--- publishconf.py
+++ publishconf.py
@@ -19,9 +19,18 @@ ANALYTICS = """
     link.textContent = "豫ICP备16018939号-2"
     document.querySelector('#contentinfo > p').prepend(link)
 </script>
-<script defer src="https://cloud.umami.is/script.js" data-website-id="084e9ce4-c530-4851-8c0f-c4dd40fb0655"></script>
-<script type="text/javascript" src="//cpro.baidustatic.com/cpro/ui/cm.js" async="async" defer="defer" >
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-J8HMB6HPES"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-J8HMB6HPES');
 </script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1314323945395441"
+    crossorigin="anonymous"></script>
+<script defer src="https://cloud.umami.is/script.js" data-website-id="084e9ce4-c530-4851-8c0f-c4dd40fb0655"></script>
 """
 DISQUS_SITENAME = "yinxianwei"
 
